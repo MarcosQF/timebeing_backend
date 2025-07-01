fab606f94e784b146571fc251871a3cb2d2626b0
Joel Júnior <joel.fmjr@gmail.com>
2025-07-01 17:54:43 -0300

    fix: fix linter errors

--- timebeing_backend/main.py
+++ timebeing_backend/main.py
@@ -8,7 +8,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'http://localhost:8080', 'https://4c15-187-19-233-163.ngrok-free.app'],
+    allow_origins=[
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:8080',
+    ],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
