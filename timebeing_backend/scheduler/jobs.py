c6f7efc47cbd0d36f08313361217ceb7265dbef9
Marcos Queiroz <marcosamfq123@gmail.com>
2025-08-01 21:11:04 -0300

    feat: changing message body to rabbitmq

--- timebeing_backend/scheduler/jobs.py
+++ timebeing_backend/scheduler/jobs.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 import aio_pika
 
+from ..auth_middleware import get_user_primary_email
 from ..settings import settings
 from .manager import scheduler
 
@@ -16,25 +17,27 @@ async def send_task_to_rabbitmq(task_data: dict):
     await connection.close()
 
 
-def schedule_notification(
-    task_title: str, due_date: datetime, notify_at: timedelta
+async def schedule_notification(
+    task_title: str, due_date: datetime, notify_at: timedelta, user_id: str
 ):
     run_time = due_date - notify_at
+
+    user_email = await get_user_primary_email(user_id=user_id)
+
     scheduler.add_job(
         send_notification_job,
         trigger='date',
         run_date=run_time,
-        args=[task_title, notify_at],
+        args=[task_title, notify_at, user_email],
         replace_existing=True,
     )
 
 
-async def send_notification_job(task_title: str, notify_at: timedelta):
-    await send_task_to_rabbitmq(
-        {
-            'message': (
-                f'Você tem {notify_at} para '
-                f'completar a task {task_title}'
-            ),
-        }
-    )
+async def send_notification_job(
+    task_title: str, notify_at: timedelta, user_email: str
+):
+    await send_task_to_rabbitmq({
+        'title': f'{task_title}',
+        'email': f'{user_email}',
+        'notify_at': f'{notify_at}',
+    })
