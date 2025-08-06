9947ca789402e3c43cdd1782f20e03068e8be5d0
Joel Júnior <joel.fmjr@gmail.com>
2025-08-06 07:14:00 -0300

    feat: Allow all origins

--- timebeing_backend/main.py
+++ timebeing_backend/main.py
@@ -23,19 +23,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://timebeing.space',
-        'https://timebeing.space',
-        'http://www.timebeing.space',
-        'https://www.timebeing.space',
-        'http://localhost',
-        'https://localhost',
-        'http://177.154.180.9'
-    ],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
